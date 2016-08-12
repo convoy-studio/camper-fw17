1d6f7ef331321addc5d1e3723252f042c9e25e00
hellopath <thomogloupanagiotis@gmail.com>
2016-08-12 12:58:26 +0300

    preloader done

--- src/js/app/desktop/index.js
+++ src/js/app/desktop/index.js
@@ -6,6 +6,7 @@ import Template from './template'
 import Router from '../../services/router'
 import { initGlobalEvents } from '../../services/global-events'
 import Preloader from '../../services/preloader'
+import dom from 'dom-hand'
 
 class App {
     constructor() {
@@ -20,6 +21,27 @@ class App {
         Store.Preloader = new Preloader()
         // Init global events
         initGlobalEvents()
+
+        this.mainLoader = dom.select('#main-loader')
+        this.mainLoader.style.opacity = 1
+        var $spinner = dom.select('.spinner-wrapper', this.mainLoader)
+        var $spinnerSvg = dom.select('svg', $spinner)
+        var $logo = dom.select('.logo', this.mainLoader)
+        var $background = dom.select('.background', this.mainLoader)
+        this.tlIn = new TimelineMax()
+        this.tlOut = new TimelineMax()
+
+        this.tlIn.fromTo($spinner, 1, {opacity:0}, { opacity:1, force3D:true, ease:Expo.easeOut }, 0)
+        this.tlIn.fromTo($logo, 1, {opacity:0}, { opacity:1, force3D:true, ease:Expo.easeOut }, 0)
+        this.tlIn.play(0)
+
+        this.spinnerTween = TweenMax.to($spinnerSvg, 0.5, { rotation:'360deg', repeat:-1, ease:Linear.easeNone })
+
+        this.tlOut.to($spinner, 1, { scale:1.2, y:10, opacity:0, force3D:true, ease:Expo.easeInOut }, 0)
+        this.tlOut.to($logo, 1, { scale:1.2, y:-10, opacity:0, force3D:true, ease:Expo.easeInOut }, 0)
+        this.tlOut.to($background, 1, { opacity:0, force3D:true, ease:Expo.easeInOut }, 0.6)
+        this.tlOut.pause(0)
+
         const appTemplate = new Template()
         appTemplate.isReady = this.loadMainAssets
         appTemplate.render('#app-container')
@@ -36,6 +58,18 @@ class App {
     onAppReady() {
         setTimeout(()=>Actions.appStart())
         setTimeout(()=>Actions.routeChanged())
+        setTimeout(()=>{
+            this.tlOut.timeScale(2).play()
+            setTimeout(() => {
+                this.tlOut.clear()
+                this.tlIn.clear()
+                this.spinnerTween.pause()
+                this.tlOut = null
+                this.tlIn = null
+                this.spinnerTween = null
+                dom.tree.remove(this.mainLoader)
+            }, 1000)
+        }, 1000)
     }
 }
 
